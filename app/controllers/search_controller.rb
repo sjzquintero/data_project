ba8d017a0b9e89c5468ad143611e4a25aacb2358
Santiago Jimenez <squintero@rrc.ca>
2024-06-27 16:59:50 -0500

    simple search fixed for cocktails search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,21 +1,21 @@
+# app/controllers/search_controller.rb
 class SearchController < ApplicationController
   def search
     @query = params[:query]
 
-
+    # Lógica de búsqueda para cocktails
     @cocktails = Cocktail.where("cocktails.name LIKE ?", "%#{@query}%")
-                         .or(Cocktail.where("cocktails.instructions LIKE ?", "%#{@query}%"))
-                         .joins(:ingredients).where("ingredients.name LIKE ?", "%#{@query}%")
-                         .distinct
 
+
+    # Lógica de búsqueda para meals
     @meals = Meal.where("meals.name LIKE ?", "%#{@query}%")
-                 .or(Meal.where("meals.instructions LIKE ?", "%#{@query}%"))
-                 .joins(:ingredients).where("ingredients.name LIKE ?", "%#{@query}%")
-                 .distinct
 
+
+    # Lógica de búsqueda para ingredients
     @ingredients = Ingredient.where("ingredients.name LIKE ?", "%#{@query}%")
 
-    @results = @cocktails + @meals + @ingredients
+    # Asignar resultados combinados
+    @results = @meals + @cocktails + @ingredients
 
     # Renderizar la vista de resultados de búsqueda
     render 'search/results'
