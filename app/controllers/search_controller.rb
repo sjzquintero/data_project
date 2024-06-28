5f11c3f518458730dcf6235a1825e7ac2daed529
Santiago Jimenez <squintero@rrc.ca>
2024-06-27 21:21:16 -0500

    small bug when sending empty search fixed

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,23 +1,19 @@
-# app/controllers/search_controller.rb
 class SearchController < ApplicationController
   def search
     @query = params[:query]
 
-    # Lógica de búsqueda para cocktails
-    @cocktails = Cocktail.where("cocktails.name LIKE ?", "%#{@query}%")
+    if @query.present?
+      @cocktails = Cocktail.where("cocktails.name LIKE ?", "%#{@query}%")
 
+      @meals = Meal.where("meals.name LIKE ?", "%#{@query}%")
 
-    # Lógica de búsqueda para meals
-    @meals = Meal.where("meals.name LIKE ?", "%#{@query}%")
+      @ingredients = Ingredient.where("ingredients.name LIKE ?", "%#{@query}%")
 
+      @results = (@cocktails + @meals + @ingredients).uniq
+    else
+      @results = []
+    end
 
-    # Lógica de búsqueda para ingredients
-    @ingredients = Ingredient.where("ingredients.name LIKE ?", "%#{@query}%")
-
-    # Asignar resultados combinados
-    @results = @meals + @cocktails + @ingredients
-
-    # Renderizar la vista de resultados de búsqueda
     render 'search/results'
   end
 end
